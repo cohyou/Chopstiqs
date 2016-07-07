c6de3b2e2b8f8c4ac4190266a50c7a7347bd9945
OKAZAKI Yasuo <yasuo_okazaki@fandmnet.com>
2016-07-07 19:11:42 +0900

    start devloping REPL

--- chopstiqs.py
+++ chopstiqs.py
@@ -1,18 +1,51 @@
 #! usr/bin/env python
+
 def clear_color():
     print('\x1b[49m', end='')
     print('\x1b[39m', end='')
 
-clear_color()
+class Tokn:
+    pass
+
+class Briq:
+    def __init__(self):
+        self.type = 'type'
+        self.mark = ''
+        self.lval = ''
+        self.gval = ''
+
+    def __repr__(self):
+        return '^[' + self.mark + ' ' + self.lval + ':' + self.gval + ']'
 
 white = '\x1b[49m'
+black_forecolor = '\x1b[30m'
 
-print(white + '\x1b[30m@\x1b[34m||' + white, end=' ')
-# print(white + '\x1b[30m@\x1b[32m|>' + white, end=' ')
-# print(white + '\x1b[30m@\x1b[31m[]' + white, end=' ')
-clear_color()
+print('itadakimasu!')
 
-inputed = input()
-print(inputed)
+while True:
+    clear_color()
+    print(white + black_forecolor + '@@\x1b[34m||' + white, end=' ')
+    # print(white + black_forecolor + '\x1b[30m@@\x1b[32m|>' + white, end=' ')
+    # print(white + black_forecolor + '\x1b[30m@@\x1b[31m[]' + white, end=' ')
+    clear_color()
 
-clear_color()
+    inputed = input()
+
+    if inputed in ['@@[]', 'gochi', '@@@', '@@quit', 'gochidesu']:
+        print('gochisosama!')
+        break
+
+    output = '('
+    for c in inputed:
+        if c == ' ':
+            output += ') ('
+        else:
+            output += c
+
+    output += ')'
+
+    b = Briq()
+
+    print(output + ' ' + repr(b))
+
+    clear_color()
