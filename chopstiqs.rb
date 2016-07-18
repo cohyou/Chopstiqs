4e70488c505ecfdc030f9fbeab20eede8b0cf53d
cohyou <cohyou@aurola.net>
2016-07-19 08:42:00 +0900

    edit ruby repl

--- chopstiqs.rb
+++ chopstiqs.rb
@@ -1,11 +1,49 @@
-line = STDIN.readline
+puts 'itadakimasu!'
 
-line.each_char do |c|
-    if c =~ /[a-zA-z]/
-        puts 'alpha'
-    else
-        puts 'not alpha'
+class Lexer
+    def initialize(input)
+        @idx = 0
+        @input = input
+        @c = input[@idx] if not input.empty?
+    end
+
+    def consume
+        @idx++
+        @c = @input[@idx]
+    end
+
+    def next_token
+        while @c != '\n'
+            case @c
+            when '"'
+                puts 'orororo'
+            else
+                puts 'other'
+            end
+            break
+        end
     end
 end
 
-print line
+while true
+    print '@|| '
+    line = STDIN.readline
+
+    lexer = Lexer.new(line)
+    lexer.next_token
+
+    line.each_char do |c|
+        if c =~ /[a-zA-z]/
+            puts 'alpha'
+        else
+            puts 'not alpha'
+        end
+    end
+
+    if line == "@@@\n"
+        puts 'gochisosama!'
+        break
+    else
+        print line
+    end
+end
